8d5c42544bdb34dc9d87c2eb8b59bc6a685edbe0
clarasantaa <c.santamariast@gmail.com>
2025-03-24 11:22:54 +0100

    Exercise B complete

--- Homeworks/splines/interpole.cs
+++ Homeworks/splines/interpole.cs
@@ -46,3 +46,90 @@ public static class interpolation{
                 return area;
         }
 } 
+
+public class qspline{
+	public vector x, y, b, c;
+	public qspline(vector xs, vector ys){
+		this.x=xs.copy();
+		this.y=ys.copy();
+		int n=xs.size;
+
+		this.b = new vector(n-1);
+		this.c = new vector(n-1);
+		vector p = new vector(n-1); /*slopes*/
+		vector h = new vector(n-1); 
+		
+		for(int i=0;i<n-1;i++){
+			h[i]=xs[i+1]-xs[i];
+			p[i]=(ys[i+1]-ys[i])/(xs[i+1]-xs[i]);
+		}
+
+		this.c[0]=0;
+		for(int i=0;i<n-2;i++){
+			this.c[i+1]=(p[i+1]-p[i]-this.c[i]*h[i])/h[i+1];
+		}
+		this.c[n-2]/=2;
+		for(int i=n-3;i>=0;i--){
+			this.c[i]=(p[i+1]-p[i]-this.c[i+1]*h[i+1])/h[i];
+		}
+
+		for(int i=0;i<n-1;i++){
+			this.b[i]=p[i]-this.c[i]*h[i];
+		}
+	}
+
+	public double evaluate(double z){
+		int n=x.size;
+        	int i=binsearch(z);
+                if(i==-1){
+                        WriteLine($"z is not in the range");
+                }
+        	return y[i]+(z-x[i])*(b[i]+c[i]*(z-x[i]));
+    	}
+
+	public double derivate(double z){
+		int n=x.size;
+		int i=binsearch(z);
+                if(i==-1){
+                        WriteLine($"z is not in the range");
+                }
+		return b[i]+2*c[i]*(z-x[i]);
+	}
+
+	public double integral(double z){
+		int n=x.size;
+		int i=binsearch(z);
+		if(i==-1){
+			WriteLine($"z is not in the range");
+		}
+		double area=0;
+		for(int k=0;k<i;k++){
+			double dx=x[k+1]-x[k];
+			area+=y[k]*dx+b[k]*dx*dx/2.0+c[k]*dx*dx*dx/3.0;
+		}
+		double dxi=z-x[i];
+		area+=y[i]*dxi+b[i]*dxi*dxi/2.0+c[i]*dxi*dxi*dxi/3.0;
+		return area;
+	}
+	private int binsearch(double z){
+    		if(z<x[0] || z>x[x.size-1]){
+        		return -1;
+		}
+ 		int i=0, j=x.size-1;
+    		while(j-i>1){
+         		int mid=(i+j)/2;
+         		if(z>x[mid]){
+             			i=mid;
+			}else{
+             			j=mid;
+    			}
+		}
+    	return i;
+	}
+}
+
+		
+
+
+
+			
--- Homeworks/splines/main.cs
+++ Homeworks/splines/main.cs
@@ -6,6 +6,8 @@ class main{
 	static int Main(){
 		vector x=new vector(10);
 		vector y=new vector(10);
+		
+		/*EXERCISE A*/
 		for(int i=0;i<10;i++){
 			x[i]=i;
 			y[i]=Math.Cos(i);
@@ -14,8 +16,23 @@ class main{
 		for(double z=x[0];z<x[x.size-1];z+=1.0/8){
 			cosz=interpolation.linterp(x,y,z);
 			areaz=interpolation.linterpInteg(x,y,z);
-		WriteLine($"{z} {cosz} {areaz}");
+			WriteLine($"{z} {cosz} {areaz}");
 		}
+		WriteLine($"\n");
+		
+		/*EXERCISE B*/
+		for(int i=0;i<10;i++){
+			y[i]=Math.Sin(i);
+		}
+		qspline spline=new qspline(x,y);
+		double sinz, derivz;
+		for(double z=x[0];z<x[x.size-1];z+=1.0/8){
+			sinz=spline.evaluate(z);
+			derivz=spline.derivate(z);
+			areaz=spline.integral(z);
+			WriteLine($"{z} {sinz} {derivz} {areaz}");
+		}
+
 		return 0;
 	}
 }
