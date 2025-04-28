1e34a4f6042d45998e0c1154b53fec78f3da7549
clarasantaa <c.santamariast@gmail.com>
2025-04-28 11:22:31 +0200

    Forced steps to circle in B

--- Homeworks/ode/main.cs
+++ Homeworks/ode/main.cs
@@ -2,6 +2,7 @@ using static System.Console;
 using System.Collections.Generic;
 using static System.Math;
 using System;
+using System.IO;
 
 class main{
 	public static vector oscillator(double x, vector y){
@@ -10,13 +11,63 @@ class main{
 		return new vector(new double[] {du,-u});
 	}
 
+	public static Func<double, vector, vector> makeoscillator(double eps){
+		return (phi, y) => {
+			double u=y[0];
+			double du=y[1];
+			return new vector(new double[] {du,1-u+eps*u*u});
+		};
+	}	
+
 	public static int Main(){
+		
+		/*EXERCISE A*/
 		vector yinit=new vector(new double[] {1.0,0.0});
 		var (xList, yList)=ODESolver.driver(oscillator,(0,10),yinit);
 		for(int i=0;i<xList.Count;i++){
 			Write($"{xList[i],10:F3} ");
 			yList[i].print();
 		}
+
+		/*EXERCISE B*/
+		double rotations=4;
+		double phiMax=2*PI*rotations;
+		
+		/*ε=0 u(0)=1 u'(0)=0*/
+		var solver1=makeoscillator(0.0);
+		vector yinit1=new vector(new double[] {1.0,0.0});
+		/*we are going to force more steps*/
+		double phi=0;
+		double h=0.001;
+		using (var circFile =new StreamWriter("circle.dat")){
+                	while(phi<=phiMax){
+				circFile.WriteLine($"{phi:F6} {yinit1[0]:F6}");
+				var dydx=solver1(phi,yinit);
+				yinit1=yinit1+h*dydx;
+				phi+=h;
+            		}
+		}
+
+		/*ε=0 u(0)=1 u'(0)=-0.5*/
+		var solver2=makeoscillator(0.0);
+		vector yinit2=new vector(new double[] {1.0,-0.5});
+		var (phis2, ys2)=ODESolver.driver(solver2,(0,phiMax),yinit2);
+		using (var ellipFile =new StreamWriter("ellipse.dat")){
+            		for (int i=0; i<phis2.Count; i++){
+                		ellipFile.WriteLine($"{phis2[i]:F6} {ys2[i][0]:F6}");
+            		}
+		}
+
+		/*ε=0.01 u(0)=1 u'(0)=-0.5*/
+		var solver3=makeoscillator(0.01);
+		vector yinit3=new vector(new double[] {1.0,-0.5});
+		var (phis3, ys3)=ODESolver.driver(solver3,(0,phiMax),yinit3);
+		using (var relaFile =new StreamWriter("relativistic.dat")){
+            		for (int i=0; i<phis3.Count; i++){
+                		relaFile.WriteLine($"{phis3[i]:F6} {ys3[i][0]:F6}");
+            		}
+		}
+
 		return 0;
 	}
 
