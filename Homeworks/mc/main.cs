5b19287ed1cab434785b5b5f05cef100320e79f6
clarasantaa <c.santamariast@gmail.com>
2025-05-04 15:34:29 +0200

    Exercise C complete

--- Homeworks/mc/main.cs
+++ Homeworks/mc/main.cs
@@ -4,13 +4,16 @@ using System;
 
 class main{
 	static int Main(string[] args){
+		
+		/*EXERCISE A*/
 		vector a =new vector(new double[] {-1,-1});
 		vector b =new vector(new double[] {1,1});
 		Func<vector,double> circle = v=>(v[0]*v[0]+v[1]*v[1]<=1.0)? 1.0 : 0.0;
+		double areaEst=0, errEst=0;
 		for(int N=10; N<=1000; N+=50){
 			var result=MC.plainmc(circle,a,b,N);
-			double areaEst=result.Item1;
-			double errEst=result.Item2;
+			areaEst=result.Item1;
+			errEst=result.Item2;
 			double errReal=Abs(areaEst-PI);
 			WriteLine($"{1/Math.Sqrt(N):F6} {errEst:F6} {errReal:F6}");
 		}
@@ -21,6 +24,23 @@ class main{
 		result1/=(PI*PI*PI);
 		err/=(PI*PI*PI);
 		WriteLine($"\n∫dx/π ∫dy/π ∫dz/π [1-cos(x)cos(y)cos(z)]⁻1 = {result1}");
+		
+		/*EXERCISE B*/
+		a =new vector(new double[] {-1,-1});
+		b =new vector(new double[] {1,1});
+		var resultQ=MC.quasimc(circle,a,b,1000);
+		double areaQ=resultQ.Item1;
+		double errQ=resultQ.Item2;
+		WriteLine($"\nMethod\t\tArea Circle\tErr Est\t\tErr Real\t");
+		WriteLine($"QuasiMC\t\t{areaQ:F6}\t{errQ:E3}\t{Math.Abs(areaQ-PI):E3}");
+		WriteLine($"PseudoRandom\t{areaEst:F6}\t{errEst:E3}\t{Math.Abs(areaEst-PI):E3}"); 
+		
+		/*EXERCISE C*/
+		var resultS=MC.stratifiedmc(circle,a,b);
+		double areaS=resultS.Item1;
+		double errS=resultS.Item2;
+		WriteLine($"StratifiedMC\t{areaS:F6}\t{errS:E3}\t{Math.Abs(areaS-PI):E3}");
 		return 0;
+	
 	}
 }	
--- Homeworks/mc/solve.cs
+++ Homeworks/mc/solve.cs
@@ -1,5 +1,6 @@
 using static System.Console;
 using static System.Math;
+using System.Collections.Generic;
 using System;
 
 public class MC{
@@ -25,4 +26,145 @@ public class MC{
 		var result=(mean*V,sigma*V/Sqrt(N));
 		return result;
 	}
+
+	public static (double,double) quasimc(Func<vector,double> f, vector a, vector b, int N){
+		int dim=a.size;
+		double V=1;
+		for(int i=0;i<dim;i++){
+			V*=b[i]-a[i];
+		}
+
+		var h1=new Halton(dim);
+		var h2=new Halton(dim+11);
+		double sum1=0, sum2=0;
+
+		for(int i=0;i<N;i++){
+			var x1=h1.get(i);
+			var x2=h2.get(i);
+			for(int k=0;k<dim;k++){
+				x1[k]=a[k]+x1[k]*(b[k]-a[k]);
+				x2[k]=a[k]+x2[k]*(b[k]-a[k]);
+			}
+			sum1+=f(x1);
+			sum2+=f(x2);
+		}
+
+		double mean=(sum1+sum2)/(2.0*N);
+		double sigma=Math.Abs(sum1-sum2)/(2.0*N);
+		return (mean*V,sigma*V/Math.Sqrt(N));
+	}
+
+	public static (double, double) stratifiedmc(Func<vector,double> f,vector a, vector b, double acc = 1e-3, double eps = 1e-3, int n_reuse =0, double mean_reuse =0){
+		int dim=a.size;
+		int N=16*dim;
+		double V=1;
+		for(int i=0;i<dim;i++){
+			V*=b[i]-a[i];
+		}
+		//Nmin = 32
+		if(N<32){
+			return plainmc(f,a,b,N);
+		}
+
+		int[] nLeft =new int[dim];
+		int[] nRight =new int[dim];
+		vector x =new vector(dim);
+		vector meanLeft =new vector(dim);
+		vector meanRight =new vector(dim);
+		double mean=0;
+
+		var rnd =new Random();
+
+		for(int i=0;i<N;i++){
+			for(int k=0;k<dim;k++){
+				x[k]=a[k]+rnd.NextDouble()*(b[k]-a[k]);
+			}
+			double fx=f(x);
+			mean+=fx;
+			for(int k=0;k<dim;k++){
+				if(x[k]>(a[k]+b[k])/2){
+					meanRight[k]+=fx;
+					nRight[k]++;
+				}else{
+					meanLeft[k]+=fx;
+					nLeft[k]++;
+				}
+			}
+		}
+
+		mean/=N;
+		for(int k=0;k<dim;k++){
+			if(nLeft[k]>0) meanLeft[k]/=nLeft[k];
+			if(nRight[k]>0) meanRight[k]/=nRight[k];
+		}
+
+		int kdiv=0;
+		double maxVar=0;
+		for(int k=1;k<dim;k++){
+			double vara=Math.Abs(meanRight[k]-meanLeft[k]);
+			if(vara>maxVar){
+				maxVar=vara;
+				kdiv=k;
+			}
+		}
+
+		double integ=(mean*N+mean_reuse*n_reuse)/(N+n_reuse)*V;
+		double error=Math.Abs(mean-mean_reuse)*V;
+		double tolerance=acc+Math.Abs(integ)*eps;
+
+		if(error<tolerance) return (integ,error);
+
+		vector a2=a.copy(), b2=b.copy();
+		double mid=(a[kdiv]+b[kdiv])/2;
+		a2[kdiv]=mid;
+		b2[kdiv]=mid;
+
+		(double integLeft, double errLeft)=stratifiedmc(f,a,b2,acc/Math.Sqrt(2),eps,nLeft[kdiv],meanLeft[kdiv]);
+		(double integRight, double errRight)=stratifiedmc(f,a2,b,acc/Math.Sqrt(2),eps,nRight[kdiv],meanRight[kdiv]);
+
+		return (integLeft+integRight, Math.Sqrt(errLeft*errLeft+errRight*errRight));
+	}		
 }
+
+public class Halton{
+	List<int> bases;
+	public Halton(int dim){
+		bases=PrimeNumbers(dim);
+	}
+	public vector get(int n){
+		int dim=bases.Count;
+		var x =new vector(dim);
+		for(int i=0;i<dim;i++){
+			x[i]=Corput(n,bases[i]);
+		}
+		return x;
+	}
+	private static double Corput(int n, int b){
+		double q=0;
+		double bk=1.0/b;
+		while(n>0){
+			q+=(n%b)*bk;
+			n/=b;
+			bk/=b;
+		}
+		return q;
+	}
+	private static List<int> PrimeNumbers(int n){
+		List<int> primes = new List<int>();
+		int candidate=2;
+		while(primes.Count<n){
+			bool isPrime=true;
+			foreach(int p in primes){
+				if(p*p>candidate) break;
+				if(candidate%p==0){
+					isPrime=false;
+					break;
+				}
+			}
+			if(isPrime) primes.Add(candidate);
+			candidate++;
+		}
+		return primes;
+	}
+}
+
