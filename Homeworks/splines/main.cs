59d42074c85aaf201cf9275a54c4ad5f564f6cb8
clarasantaa <c.santamariast@gmail.com>
2025-06-25 17:59:42 +0200

    Separated data

--- Homeworks/splines/main.cs
+++ Homeworks/splines/main.cs
@@ -1,5 +1,6 @@
 using static System.Console;
 using static System.Math;
+using System.IO;
 using System;
 
 class main{
@@ -13,36 +14,40 @@ class main{
 			y[i]=Math.Cos(i);
 		}
 		double cosz, areaz;
-		for(double z=x[0];z<x[x.size-1];z+=1.0/8){
-			cosz=interpolation.linterp(x,y,z);
-			areaz=interpolation.linterpInteg(x,y,z);
-			WriteLine($"{z} {cosz} {areaz}");
-		}
-		WriteLine($"\n");
 		
+		using(var File =new StreamWriter("out.linear.txt")){
+			for(double z=x[0];z<x[x.size-1];z+=1.0/8){
+				cosz=interpolation.linterp(x,y,z);
+				areaz=interpolation.linterpInteg(x,y,z);
+				File.WriteLine($"{z} {cosz} {areaz}");
+			}
+		}
+
 		/*EXERCISE B*/
 		for(int i=0;i<10;i++){
 			y[i]=Math.Sin(i);
 		}
 		qspline spline=new qspline(x,y);
 		double sinz, derivz;
-		for(double z=x[0];z<x[x.size-1];z+=1.0/8){
-			sinz=spline.evaluate(z);
-			derivz=spline.derivate(z);
-			areaz=spline.integral(z);
-			WriteLine($"{z} {sinz} {derivz} {areaz}");
+		using(var File =new StreamWriter("out.qspline.txt")){
+			for(double z=x[0];z<x[x.size-1];z+=1.0/8){
+				sinz=spline.evaluate(z);
+				derivz=spline.derivate(z);
+				areaz=spline.integral(z);
+				File.WriteLine($"{z} {sinz} {derivz} {areaz}");
+			}
 		}
-		WriteLine($"\n");
 
 		/*EXERCISE C*/
                 cspline splinec=new cspline(x,y);
-                for(double z=x[0];z<x[x.size-1];z+=1.0/8){
-                        sinz=splinec.evaluate(z);
-                        derivz=splinec.derivate(z);
-                        areaz=splinec.integral(z);
-                        WriteLine($"{z} {sinz} {derivz} {areaz}");
-                }
-
+		using(var File =new StreamWriter("out.cspline.txt")){
+                	for(double z=x[0];z<x[x.size-1];z+=1.0/8){
+                        	sinz=splinec.evaluate(z);
+                        	derivz=splinec.derivate(z);
+                        	areaz=splinec.integral(z);
+                        	File.WriteLine($"{z} {sinz} {derivz} {areaz}");
+                	}
+		}
 
 		return 0;
 	}
