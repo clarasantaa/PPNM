eead573b5ae0121696b326c38ee89e6fabea0e5c
clarasantaa <c.santamariast@gmail.com>
2025-06-25 21:18:28 +0200

    Exercise C complete

--- Homeworks/ode/main.cs
+++ Homeworks/ode/main.cs
@@ -73,11 +73,73 @@ class main{
 			new vector(new double[]{y[1],2*x});
 		vector yinitC=new vector(new double[]{0.0,0.0});
 		var (xC, yC, hC) =ODESolver.driver23(f,(0.0,1.0),yinitC,0.1);
-		WriteLine($"\n");
-		for(int i=0;i<xC.Count;i++){
-		//	Write($"{hC[i],10:F3} {xC[i],10:F3}" );
-		//	yC[i].print();
+		using (var File=new StreamWriter("out.rk23.dat")){
+			File.WriteLine($"\th\t x\ty_num\ty_exact\terror");
+			for(int i=0;i<xC.Count;i++){
+				double ynum=yC[i][0];
+				double yexact=Pow(xC[i],3)/3.0;
+				File.WriteLine($"{hC[i],10:F3} {xC[i],10:F3}\t{ynum:F3}\t{yexact:F3}\t{Abs(ynum-yexact):E6}");
+			}
 		}
+		
+		var qspline=ODESolver.make_ode_ivp_qspline(f,(0.0,1.0),yinitC);
+		double x_eval=0.5;
+		vector y_interp=qspline(x_eval);
+
+		double y_exact=Pow(x_eval,3)/3.0, dy_exact=Pow(x_eval,2);
+
+		WriteLine($"\n\nInterpolated values of y''=2x with y(0) = 0 y'(0) = 0");
+		WriteLine($"y({x_eval}) = {y_interp[0]:F5} , exact = {y_exact:F5}, error = {Math.Abs(y_interp[0] - y_exact):E2}");
+		WriteLine($"y'({x_eval}) = {y_interp[1]:F5} , exact = {dy_exact:F5}, error = {Math.Abs(y_interp[1] - dy_exact):E2}");
+		
+		Func<double,vector,vector> threebody = (t,z) =>{
+			var dz =new vector(12);
+			vector r1 =new vector(new double[] {z[6],z[7]});
+			vector r2 =new vector(new double[] {z[8],z[9]});
+			vector r3 =new vector(new double[] {z[10],z[11]});
+			
+			vector v1 =new vector(new double[] {z[0],z[1]});
+			vector v2 =new vector(new double[] {z[2],z[3]});
+			vector v3 =new vector(new double[] {z[4],z[5]});
+
+			vector r12=r2-r1;
+			vector r13=r3-r1;
+			vector r23=r3-r2;
+
+			double r12len=r12.norm(), r13len=r13.norm(), r23len=r23.norm();
+
+			vector a1=r12/Pow(r12len,3)+r13/Pow(r13len,3);
+			vector a2=-r12/Pow(r12len,3)+r23/Pow(r23len,3);
+			vector a3=-r13/Pow(r13len,3)-r23/Pow(r23len,3);
+
+			dz[0]=a1[0];
+			dz[1]=a1[1];
+			dz[2]=a2[0];
+			dz[3]=a2[1];
+			dz[4]=a3[0];
+			dz[5]=a3[1];
+			dz[6]=v1[0];
+			dz[7]=v1[1];
+			dz[8]=v2[0];
+			dz[9]=v2[1];
+			dz[10]=v3[0];
+			dz[11]=v3[1];
+			
+			return dz;
+		};
+		
+		vector z0 = new vector(0.4662036850,0.4323657300,-0.93240737,-0.86473146,0.4662036850,0.4323657300,-0.97000436,0.24308753,0.0,0.0,0.97000436,-0.24308753);
+		
+		double t_eval=6.3259; //we integrate over one third of a period
+		var(xT,yT)=ODESolver.driver(threebody,(0,t_eval/3),z0,0.001,1e-4,1e-4);
+
+		using (var File =new StreamWriter("out.threebody.dat")){
+			for(int i=0;i<xT.Count;i++){
+			vector zi=yT[i];
+			File.WriteLine($"{zi[6]} {zi[7]} {zi[8]} {zi[9]} {zi[10]} {zi[11]}");
+			}
+		}
+
 		return 0;
 	}
 
--- Homeworks/ode/solve.cs
+++ Homeworks/ode/solve.cs
@@ -13,12 +13,11 @@ public class ODESolver{
 	}
 
 	public static (vector, vector) rkstep23(Func<double,vector,vector> f, double x, vector y, double h){
-		vector k0=f(x,y);
-		vector k1=f(x+h/2,y+k0*(h/2));
-		vector k2=f(x+3.0/4*h,k1*3.0/4*h);
-		vector k3=f(x+h,y+h*(2.0/9*k0+3.0/9*k1+4.0/9*k2));
-		vector yh3=y+h*(2.0/9*k0+3.0/9*k1+4.0/9*k2);
-		vector yh2=y+h*(7.0/24*k0+1.0/4*k1+1.0/3*k2+1.0/8*k3);
+		vector k1=f(x,y);
+		vector k2=f(x+h/2,y+k1*(h/2));
+		vector k3=f(x+3.0/4*h,y+k2*3.0/4*h);
+		vector yh3=y+h*(2.0/9*k1+3.0/9*k2+4.0/9*k3);
+		vector yh2=y+h*(7.0/24*k1+1.0/4*k2+1.0/3*k3);
 		vector dy=yh3-yh2;
 		return (yh3,dy);
 	}
@@ -82,11 +81,65 @@ public class ODESolver{
 			/*acc=eps=0 always*/
 			x=x+h;
 			y=yh3;
-			h*=2;
 			xList.Add(x);
 			yList.Add(y);
 			hList.Add(h);
+			h*=2;
 		}while(true);
 	}
 
+	public static int binsearch(List<double> x, double z){
+		int i=0, j=x.Count-1;
+		while (j-i>1){
+			int mid=(i+j)/2;
+			if(z>x[mid]) i=mid;
+			else j=mid;
+		}
+		return i;
+	}
+
+	public static Func<double, vector> make_qspline(List<double> x, List<vector> y){
+		int n=x.Count;
+		List<vector> b =new List<vector>();
+		List<vector> c =new List<vector>();
+
+		for(int i=0;i<n-1;i++){
+			double dx=x[i+1]-x[i];
+			vector dy=y[i+1]-y[i];
+			vector bi=dy/dx;
+			b.Add(bi);
+		}
+
+		c.Add(new vector(y[0].size));
+		for(int i=0;i<n-2;i++){
+			double dx1=x[i+1]-x[i];
+			double dx2=x[i+2]-x[i+1];
+			vector c_next=(b[i+1]-b[i]-c[i]*dx1)/dx2;
+			c.Add(c_next);
+		}
+
+		Func<double,vector> qspline = delegate(double z){
+			int i=binsearch(x,z);
+			if(i<0||i>=x.Count-1){
+				WriteLine($"Invalid index: {i}, z = {z}, x.Count = {x.Count}");
+			}
+			double dx=z-x[i];
+			return y[i]+dx*(b[i]+c[i]*dx);
+		};
+		
+		return qspline;
+	}
+
+	public static Func<double,vector> make_ode_ivp_qspline(Func<double,vector,vector> F, (double,double) interval, vector yinit, double acc=0.01, double eps=0.01, double hstart=0.01){
+		var(xList, yList) =driver(F,interval,yinit,hstart,acc,eps);
+		List<double> xgen =new List<double>();
+		List<vector> ygen =new List<vector>();
+		for(int i=0;i<xList.Count;i++){
+			xgen.Add(xList[i]);
+			ygen.Add(yList[i]);
+		}
+	
+		return make_qspline(xgen,ygen);
+	}
+	
 }
